7dd33370a63cf1aabb3542bd3c6dec135bd67b0e
Aravindan22 <aravindan7822@gmail.com>
2021-10-20 02:41:16 +0000

    Initail Live data Play

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ const express = require('express')
 const app= express()
 
 const tvr = require('trading-view-recommends-parser-nodejs')
+
+
+var marketAPI = require("@mathieuc/tradingview");
+
+(async () => {
+  const market = marketAPI();
+
+  market.on('logged', async () => {
+    console.log('API LOGGED');
+
+    const searchBTC = (await market.search('STPT USDT', 'crypto'))[0];
+    console.log('Found STPT / USDT:', searchBTC);
+    market.subscribe(searchBTC.id);
+  });
+
+  market.on('price', (data) => {
+    console.log(data.symbol, '=>', data.price);
+  });
+
+  
+
+  
+})();
+
+
 var result='';
 async function t() {
      result = await new tvr.TradingViewScan(
@@ -17,4 +42,4 @@ async function t() {
 // var intervalId = setInterval(function(){
 //     t()
 //   }, 100000);
-t()
+// t()
