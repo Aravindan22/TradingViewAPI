b9e455f5d56b01a53d088d0602312b2963782c30
Aravindan22 <aravindan7822@gmail.com>
2021-10-21 01:47:18 +0000

    Added Master

--- index.js
+++ index.js
@@ -1,45 +1,91 @@
-const express = require('express')
-const app= express()
+const express = require("express");
+const app = express();
 
-const tvr = require('trading-view-recommends-parser-nodejs')
+const tvr = require("trading-view-recommends-parser-nodejs");
+const marketAPI = require("@mathieuc/tradingview");
 
-
-var marketAPI = require("@mathieuc/tradingview");
+var prev_price = 0;
+var profit = 0;
+var flag = false;
+var bought_price = 0;
+var no_of_coins = 0;
 
 (async () => {
   const market = marketAPI();
 
-  market.on('logged', async () => {
-    console.log('API LOGGED');
+  market.on("logged", async () => {
+    console.log("API LOGGED");
 
-    const searchBTC = (await market.search('STPT USDT', 'crypto'))[0];
-    console.log('Found STPT / USDT:', searchBTC);
+    const searchBTC = (await market.search("STPT USDT", "crypto"))[0];
+    console.log("Found STPT / USDT:", searchBTC);
     market.subscribe(searchBTC.id);
   });
 
-  market.on('price', (data) => {
-    console.log(data.symbol, '=>', data.price);
+  market.on("price", (data) => {
+    // console.log(data.symbol, '=>', data.price);
+    cal(data.price, data.symbol);
   });
-
-  
-
-  
 })();
 
+function cal(cur_price, symbol) {
+  var prev_profit = 0;
+  if (flag == true) {
+    //   prev_profit = profit;
+    // profit += (
+    //   (cur_price - prev_price)*no_of_coins
+    // )
 
-var result='';
+    master(cur_price);
+    // console.log("Current Price :",cur_price,"| Bought on :",bought_price,"| Profit:",(cur_price*no_of_coins)-(bought_price * no_of_coins));
+    console.log("Asset Prev Value :", bought_price * no_of_coins, "| Current Asset Value :", cur_price * no_of_coins, "| Percentage diff",  (cur_price * 100) / bought_price - 100 );
+    
+  } 
+  else {
+    buyOrSell().then((result) => {
+      if (result == true) {
+        flag = true;
+        bought_price = cur_price;
+        no_of_coins = 10 / cur_price;
+        console.log("Number Of coins =>", no_of_coins);
+        prev_price = cur_price;
+      }
+    });
+  }
+}
+
+async function buyOrSell() {
+  return t().then((result) => {
+    console.log(result);
+    if (result == "BUY") {
+      return true;
+    } else return false;
+  });
+}
+
+function master(cur_price) {
+  if ((cur_price * 100) / bought_price - 100 <= -2) {
+    console.log("SELL All the Coins | Asset Sold :", cur_price * no_of_coins);
+  } else if ((cur_price * 100) / bought_price - 100 > 2) {
+    console.log(
+      "SELL ALL Coins Profit | Asset Sold :",
+      cur_price * no_of_coins
+    );
+  }
+}
+
+var result = "";
 async function t() {
-     result = await new tvr.TradingViewScan(
-        tvr.SCREENERS_ENUM['crypto'],
-        tvr.EXCHANGES_ENUM['BINANCE'],
-        'STPTUSDT',
-        tvr.INTERVALS_ENUM['1m'],
-        // You can pass axios instance. It's optional argument (you can use it for pass custom headers or proxy)
-      ).analyze();
-    //   console.log(result.oscillators['RECOMMENDATION']);
-    console.log(JSON.stringify(result));
+  result = await new tvr.TradingViewScan(
+    tvr.SCREENERS_ENUM["crypto"],
+    tvr.EXCHANGES_ENUM["BINANCE"],
+    "STPTUSDT",
+    tvr.INTERVALS_ENUM["1m"]
+    // You can pass axios instance. It's optional argument (you can use it for pass custom headers or proxy)
+  ).analyze();
+  // console.log(result.oscillators['RECOMMENDATION']);
+  // console.log(JSON.stringify(result));
+  return result.oscillators["RECOMMENDATION"];
 }
 // var intervalId = setInterval(function(){
 //     t()
 //   }, 100000);
-// t()
