69d8921468cd90d75d087a86e7d1b7af1b36955d
Aravindan22 <aravindan7822@gmail.com>
2021-10-22 05:59:57 +0000

    Corrected Profit/Losss Calculation

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ var profit = 0;
 var flag = false;
 var bought_price = 0;
 var no_of_coins = 0;
-var asset_value=0;
-var total_profit=0;
+var asset_value = 0;
+var total_profit = 0;
+var no_of_ProfitTrades = 0;
+var no_of_LossTrades = 0;
+var result = "";
+
 (async () => {
   const market = marketAPI();
 
@@ -38,10 +42,16 @@ function cal(cur_price, symbol) {
 
     master(cur_price);
     // console.log("Current Price :",cur_price,"| Bought on :",bought_price,"| Profit:",(cur_price*no_of_coins)-(bought_price * no_of_coins));
-    console.log("Asset Prev Value :", bought_price * no_of_coins, "| Current Asset Value :", cur_price * no_of_coins, "| Percentage diff",  (cur_price * 100) / bought_price - 100 );
-    
-  } 
-  else {
+    console.log(
+      "Asset Prev Value :",
+      bought_price * no_of_coins,
+      "| Current Asset Value :",
+      cur_price * no_of_coins,
+      "| Percentage diff",
+      (cur_price * 100) / bought_price - 100
+    );
+  } else {
+    console.log("Calling BuyOrSell");
     buyOrSell().then((result) => {
       if (result == true) {
         flag = true;
@@ -56,7 +66,7 @@ function cal(cur_price, symbol) {
 }
 
 async function buyOrSell() {
-  return t().then((result) => {
+  return recommendation().then((result) => {
     console.log(result);
     if (result == "BUY") {
       return true;
@@ -65,20 +75,24 @@ async function buyOrSell() {
 }
 
 function master(cur_price) {
-  if ((cur_price * 100) / bought_price - 100 <= -1) {
-    console.log("SELL All the Coins | Asset Sold :", cur_price * no_of_coins);
-    total_profit += asset_value - (cur_price*no_of_coins);
-    no_of_coins=0;
-  } else if ((cur_price * 100) / bought_price - 100 > 2) {
+  if ((cur_price * 100) / bought_price - 100 <= -0.5 && asset_value > 0) {
+    console.log("SELL All the Coins on LOSS| Asset Sold :",cur_price * no_of_coins );
+    total_profit += (cur_price * no_of_coins)-asset_value;
+    no_of_coins = 0;
+    no_of_LossTrades += 1;
+    flag=false;
+  } else if ((cur_price * 100) / bought_price - 100 > 0.5) {
     console.log("SELL ALL Coins Profit | Asset Sold :",cur_price * no_of_coins);
-    total_profit += asset_value - (cur_price*no_of_coins);
-    no_of_coins=0;
+    total_profit += (cur_price * no_of_coins)-asset_value;
+    no_of_coins = 0;
+    no_of_ProfitTrades += 1;
+    flag=false;
   }
-  asset_value=no_of_coins*cur_price;
+  asset_value = no_of_coins * cur_price;
 }
 
-var result = "";
-async function t() {
+
+async function recommendation() {
   result = await new tvr.TradingViewScan(
     tvr.SCREENERS_ENUM["crypto"],
     tvr.EXCHANGES_ENUM["BINANCE"],
@@ -86,25 +100,31 @@ async function t() {
     tvr.INTERVALS_ENUM["1m"]
     // You can pass axios instance. It's optional argument (you can use it for pass custom headers or proxy)
   ).analyze();
-  // console.log(result.oscillators['RECOMMENDATION']);
-  // console.log(JSON.stringify(result));
+
   return result.oscillators["RECOMMENDATION"];
 }
-// var intervalId = setInterval(function(){
-//     t()
-//   }, 100000);
-app.get("/",function (req,res) {
 
-
-  let s =`<!DOCTYPE html>  <html lang="en"> <head>
+app.get("/", function (req, res) {
+  let s =
+    `<!DOCTYPE html>  <html lang="en"> <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Tradey</title>
     </head>
     <body>
-    <h1>Asset Value :`+asset_value+`</h1>
-    <h1>Total Profit : `+total_profit+`</h1>
+    <h3>Asset Value :` +
+    asset_value +
+    `</h3>
+    <h3>Total Profit : ` +
+    total_profit +
+    `</h3>
+    <h3>Profit Trades : ` +
+    no_of_ProfitTrades +
+    `</h3>
+    <h3>Loss Trades : ` +
+    no_of_LossTrades +
+    `</h3>
     <script>
     window.setInterval('refresh()', 2000); 	
     // Call a function every 1000 milliseconds 
@@ -116,10 +136,10 @@ app.get("/",function (req,res) {
     }
     </script>
     </body>
-  </html>`
+  </html>`;
   // let s = "<h1>Asset Value :" +asset_value+"</h1><h1>Total Profit : "+total_profit+"</h1>";
   res.send(s);
-})
-app.listen(process.env.PORT || 3000,function (req,res) {
+});
+app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Listening @3000");
-})
+});
